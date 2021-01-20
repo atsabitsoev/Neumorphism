9f065b3480f2279bb4795bed2bbe2d0b183729bd
Ацамаз Бицоев <atsamazbitsoev@yandex.ru>
2021-01-20 13:32:35 +0300

    Initial Commit

---
+++ Neumorphism/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Neumorphism
+//
+//  Created by Ацамаз Бицоев on 20.01.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Neumorphism/NeumorphismApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeumorphismApp.swift
+//  Neumorphism
+//
+//  Created by Ацамаз Бицоев on 20.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct NeumorphismApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
